3a66a071f6793b7c4ef02714c4a10f9d489db205
Ro11ingButler <rollingkai1214@gmail.com>
2023-11-08 10:16:42 +0800

    Add google to remotePatterns

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/storage/v1/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
